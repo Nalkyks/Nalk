47ec43864cfede610602320216dc0e4708c35bff
Nalkyks <49298437+Nalkyks@users.noreply.github.com>
2019-12-19 22:58:40 -0700

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -18,8 +18,8 @@ class KickCommand extends Commando.Command
         if(!message.member.hasPermission('KICK_MEMBERS'))
         {
             let invalidPermission = new Discord.RichEmbed()
-                .setTitle('Invalid Permissions')
-                .setDescription("You do not have permission to use the `kick` command. Contact the server/guild owner or an administrator/moderator for help.")
+                .setTitle('Error: Invalid Permissions')
+                .setDescription("You do not have permission to use the `kick` command. Think this is a mistake? Contact the server/guild owner or an administrator/moderator for assistance.")
                 .setThumbnail('https://bit.ly/2DRDdkA')
                 .setColor(0xFF0000)
                 .setFooter('Requested by: ' + message.author.username)
@@ -31,7 +31,7 @@ class KickCommand extends Commando.Command
         if(!kickedUser)
         {
             let invalidUser = new Discord.RichEmbed()
-                .setTitle('Member Not Found')
+                .setTitle('Error: Member Not Found')
                 .setDescription("The requested user to be kicked is invalid. User may have left the server before you could've kicked them, or you made a typo.")
                 .setThumbnail('https://bit.ly/2DRDdkA')
                 .setColor(0xFF0000)
@@ -45,8 +45,8 @@ class KickCommand extends Commando.Command
         if(!reason)
         {
             let invalidReason = new Discord.RichEmbed()
-                .setTitle('No Reason Was Specified')
-                .setDescription("No reason was specified on why " + kickedUser + " is getting kicked. Please provide a reason on why " + kickedUser + " is getting kicked.")
+                .setTitle('Error: Invalid Reason')
+                .setDescription("No reason was specified on why " + kickedUser + " is getting kicked, please provide a reason on why " + kickedUser + " is getting kicked and try again.")
                 .setThumbnail('https://bit.ly/2DRDdkA')
                 .setColor(0xFF0000)
                 .setFooter('Requested by: ' + message.author.username)
@@ -60,12 +60,12 @@ class KickCommand extends Commando.Command
         message.delete();
         let kickEmbed = new Discord.RichEmbed()
             .setTitle("Kick Details")
-            .setDescription("These are the kick details for " + kickedUser + ", and gives you more information about the kicked user.")
+            .setDescription("These are the kick details for " + kickedUser + ", and gives you more information about the kicked user and the administrator/moderator.")
             .setColor(0xFFFF00)
             .addField("Kicked User:", kickedUser + " with ID: " + kickedUser.id)
             .addField("Kicked By:", message.author + " with ID: " + message.author.id)
             .addField("Kicked On:", message.createdAt)
-            .addField("Reason for Kick:", reason)
+            .addField("Moderator Note:", reason)
         message.channel.sendEmbed(kickEmbed);
         console.log("The 'kick' command has been completed successfully."); 
     }
